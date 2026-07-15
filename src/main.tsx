e7ec7d1f3d995ccd43f22bea81a5b88a9865cc52
Anastasia Ded <anastasiaded@192.168.2.4>
2026-07-15 22:30:20 +0300

    Fix label/border overlap caused by font-loading race

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,26 @@ import { ThemeProvider, CssBaseline } from "@mui/material";
 import App from "./App";
 import { theme } from "./theme";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+function renderApp() {
+  root.render(
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+}
+
+const FONT_LOAD_TIMEOUT_MS = 300;
+
+if ("fonts" in document) {
+  Promise.race([
+    document.fonts.ready,
+    new Promise((resolve) => setTimeout(resolve, FONT_LOAD_TIMEOUT_MS)),
+  ]).then(renderApp);
+} else {
+  renderApp();
+}
